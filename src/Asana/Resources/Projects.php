68d476144cabca43a09adde4e1ddc57e2df26419
Fraser Graham <fgraham@revenuewire.com>
2018-04-20 15:33:13 -0700

    Added createSectionInProject method

--- src/Asana/Resources/Projects.php
+++ src/Asana/Resources/Projects.php
@@ -6,4 +6,9 @@ use Asana\Resources\Gen\ProjectsBase;
 
 class Projects extends ProjectsBase
 {
+    public function createSectionInProject($projectId, $params = array())
+    {
+        $path = sprintf("/projects/%s/sections", $projectId);
+        return $this->client->post($path, $params);
+    }
 }
